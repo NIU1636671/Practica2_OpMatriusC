efc660f1fbbf02f480741b08d369215783c1f1d0
SenshiPRC <57967962+SenshiPRC@users.noreply.github.com>
2021-11-08 10:00:57 +0100

    Exercici 1, mulMat()

--- practica1.c
+++ practica1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-const int N = 512;
-int Mat1[N][N], Mat2[N][N];
-int Vect1[N], Vect2[N];
+#include <stdlib.h>
+#define N 512;
+int Mat1[512][512], Mat2[512][512], MatRes[512][512];
+int Vect1[512], Vect2[512];
 
-void main()
+int main()
 {
     initMats();
+    mulMat(Mat1, Mat2, MatRes);
+    imprimirMatriu(MatRes);
 }
 void initMats()
 {
@@ -24,6 +27,31 @@ void initMats()
     }
 }
 
-int mulMat(int Mat1, int Mat2, int MatRes)
+int mulMat(int Mat1[512][512], int Mat2[512][512], int MatRes[512][512])
 {
+    int i, j, k;
+    for (i = 0; i < 512; i++)
+    {
+        for (j = 0; j < 512; j++)
+        {
+            MatRes[i][j] = 0;
+            for (k = 0; k < 512; k++)
+            {
+                MatRes[i][j] += Mat1[i][k] * Mat2[k][j];
+            }
+        }
+    }
+}
+
+void imprimirMatriu(int MatRes[512][512])
+{
+    int fila, columna;
+    for (fila = 0; fila < 4; fila++)
+    {
+        for (columna = 0; columna < 4; columna++)
+        {
+            printf("%d     ", MatRes[fila][columna]);
+        }
+        printf("\n");
+    }
 }
