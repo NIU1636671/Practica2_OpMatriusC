d69ba72fbe8b664cc7290d162ebd92ccbe3da4e7
SenshiPRC <57967962+SenshiPRC@users.noreply.github.com>
2021-11-12 11:59:09 +0100

    Afegir ; que faltava

--- practica1.c
+++ practica1.c
@@ -66,7 +66,7 @@ int sumDiagonal(int Matriu[512][512])
     int i;
     for (i = 0; i < 512; i++)
         sumaDiag = sumaDiag + Matriu[i][i];
-    return sumaDiag
+    return sumaDiag;
 }
 
 /*Imprimir matrius*/
